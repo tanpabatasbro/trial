88dcebd8b1ef03b914612bd40d08d21e8c1dd544
tanpabatasbro <115479649+tanpabatasbro@users.noreply.github.com>
2022-10-17 20:37:08 +0700

    Add files via upload

---
+++ trial 1.py
@@ -0,0 +1,9 @@
+nama = input("masukkan nama :")
+jenis_kelamin = input("masukkan jenis kelamin :")
+umur = int(input("masukkan umur (dalam bentuk angka) :"))
+alamat = input("masukkan alamat tempat tinggal anda :")
+
+print("perkenalkan nama saya adalah {}".format(nama))
+print("saya berjenis kelamin {}".format(jenis_kelamin))
+print("saya berusia", umur, "tahun")
+print("saya tinggal di {}".format(alamat))
